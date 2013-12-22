10f93dd1825eaa96d5ad68baf03cdd0e7d27d834
Jeff Miller <jeffmiller@jeffmiller.name>
2013-12-22 11:00:53 -0500

    added field and shadow casting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,11 +8,13 @@ var VIEW_ANGLE = 45,
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-camera.position.set(0, -10, 5);
+var camera_offset = -8.0
+camera.position.set(0, camera_offset, camera_offset / -2.0);
 camera.lookAt(scene.position);
-// camera.position.y -= 20;
 
 var renderer = new THREE.WebGLRenderer();
+renderer.shadowMapEnabled = true;
+renderer.shadowMapSoft = true;
 renderer.setSize(getViewportWidth(), getViewportHeight());
 document.body.appendChild(renderer.domElement);
 
@@ -20,17 +22,20 @@ var field = getField();
 scene.add( field );
 
 var ball = getBall();
-ball.position.z += 0.5;
 scene.add( ball );
 
 // add subtle blue ambient lighting
-var ambientLight = new THREE.AmbientLight(0x9999EE);
+var ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
-// directional lighting
-var directionalLight = new THREE.DirectionalLight(0x999999);
-directionalLight.position.set(ASPECT, 1, 2).normalize();
+var light_x = 5, light_y = 0, light_z = 5;
+
+var directionalLight = getDirectionalLight(light_x, light_y, light_z);
 scene.add(directionalLight);
+
+var light = getLight();
+light.position.set(light_x, light_y, light_z);
+scene.add(light);
 
 window.addEventListener('resize', function(e) {
 	camera.aspect = getViewportWidth() / getViewportHeight();
@@ -40,20 +45,6 @@ window.addEventListener('resize', function(e) {
 
 	render();
 });
-
-function mouseMove(e){
-  mouse2D.x = ( (e.pageX-canvas.offsetParent.offsetLeft) / canvas.clientWidth ) * 2 - 1;
-  mouse2D.y = - ( (e.pageY-canvas.offsetParent.offsetTop) / canvas.clientHeight ) * 2 + 1;
-
-  if (selected) {
-    var ray = projector.pickingRay(mouse2D, PGL._camera).ray;
-    var targetPos = ray.direction.clone().addSelf(ray.origin);
-    targetPos.subSelf(_offset);
-
-    obj.position.x = initPos.x + targetPos.x;
-    obj.position.y = initPos.y + targetPos.y;
-  }
-}
 
 function render() {
 	renderer.render( scene, camera );
@@ -71,28 +62,72 @@ window.onload = function() {
   render();
 }
 
-function getBall() {
-  var geometry = new THREE.SphereGeometry(0.5,30,30);
+function getLight() {
+  var geometry = new THREE.SphereGeometry(0.2,30,30);
 
-  var texture = THREE.ImageUtils.loadTexture('images/ball.jpg');
-  texture.anisotropy = renderer.getMaxAnisotropy();
-
-  var material = new THREE.MeshPhongMaterial({map: texture});
+  var material = new THREE.MeshBasicMaterial({color: 0xFFFF00});
 
   var sphere = new THREE.Mesh(geometry, material);
 
   return sphere;
 }
 
+function getBall() {
+  var geometry = new THREE.SphereGeometry(0.1,30,30);
+
+  var texture = THREE.ImageUtils.loadTexture('images/ball.jpg');
+  texture.anisotropy = renderer.getMaxAnisotropy();
+
+  var material = new THREE.MeshPhongMaterial({map: texture});
+
+  var object = new THREE.Mesh(geometry, material);
+
+  object.position.z += 0.1;
+  object.castShadow = true;
+  object.receiveShadow = false;
+
+  return object;
+}
+
 function getField() {
-  var geometry = new THREE.PlaneGeometry(10,10);
+  var geometry = new THREE.PlaneGeometry(16,9);
 
-  // var texture = THREE.ImageUtils.loadTexture('images/ball.jpg');
-  // texture.anisotropy = renderer.getMaxAnisotropy();
+  var texture = THREE.ImageUtils.loadTexture('images/field.jpg');
+  texture.anisotropy = renderer.getMaxAnisotropy();
 
-  var material = new THREE.MeshBasicMaterial({color: '#006600'});
+  var material = new THREE.MeshPhongMaterial({
+    map: texture,
+    ambient: 0x030303, 
+    specular: 0x00FF00, 
+    shininess: 5, 
+    shading: THREE.FlatShading
+  });
 
-  var plane = new THREE.Mesh(geometry, material);
+  var object = new THREE.Mesh(geometry, material);
 
-  return plane;
+  object.castShadow = false;
+  object.receiveShadow = true;
+
+  return object;
+}
+
+function getDirectionalLight(x, y, z) {
+  // directional lighting
+  var directionalLight = new THREE.DirectionalLight(0xEEEEEE, 0.7);
+  directionalLight.target.position.copy( scene.position );
+  directionalLight.castShadow = true;
+  directionalLight.shadowDarkness = 0.7;
+  directionalLight.shadowCameraVisible = true;
+
+  directionalLight.shadowCameraLeft = -5;
+  directionalLight.shadowCameraTop = -5;
+  directionalLight.shadowCameraRight = 5;
+  directionalLight.shadowCameraBottom = 5;
+  directionalLight.shadowCameraNear = 0.01;
+  directionalLight.shadowCameraFar = 100;
+  directionalLight.shadowBias = -.0001;
+  directionalLight.shadowMapWidth = directionalLight.shadowMapHeight = 2048;
+
+  directionalLight.position.set(x, y, z);
+  return directionalLight;
 }
